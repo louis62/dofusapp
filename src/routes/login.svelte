7de5897b0e331661c8b49c2baa5adbad5e104beb
Louis Boutesocq <louis.boutesocq@live.fr>
2021-02-18 17:41:47 +0100

    Add preprocessor

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -15,30 +15,30 @@
     </div>
 </div>
 
-<style scoped>
+<style scoped>   
+ @import ".././theme/variables.scss";
     .bg {
         min-height: 100%;
         width: 100%;
-        background-color: #33425b;
+        background-color: $primary;
         display: flex;
         justify-content: center;
         align-items: center;
+        .wrapper{
+            flex-wrap: wrap;
+            max-width: 80vw;
+            .form__wrapper{
+                background: $secondary;
+                border-radius: 20px;
+                margin-top: -3em;
+                background: #33425b;
+                box-shadow:  5px 5px 10px #303e56,
+                            -5px -5px 10px #364660;
+                padding: 1em;
+            }
+            img {
+                max-width: 80vw;
+            }
+        }
     }
-    .wrapper {
-        flex-wrap: wrap;
-        max-width: 80vw;
-    }
-    .wrapper img {
-        max-width: 80vw; 
-    }
-    .form__wrapper {
-        background: #d8e9f0;
-        border-radius: 20px;
-        margin-top: -3em;
-        background: #33425b;
-        box-shadow:  5px 5px 10px #303e56,
-                    -5px -5px 10px #364660;
-        padding: 1em;
-    }
-    
 </style>
